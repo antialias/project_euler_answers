942324820cfd50237522bb17b2f68580b64383d4
Thomas Hallock <github@thomashallock.com>
2013-05-21 01:05:04 -0400

    removed unnecessary cruft

--- euler1.py
+++ euler1.py
@@ -3,14 +3,8 @@ b1 = 3.0
 b2 = 5.0
 n = 10000.0
 
-# print ((n-1) / b1)*(((n-1)/b1)-1)/2 * b1 + ((n-1) / b2)*(((n-1)/b2)-1)/2 * b2
 print "smart: {smart}".format(smart=math.floor((n-1)/b2)*((math.floor((n-1)/b2) + 1))/2 * b2 + math.floor((n-1)/b1)*((math.floor((n-1)/b1) + 1))/2 * b1 - math.floor((n-1)/(b1*b2))*((math.floor((n-1)/(b1*b2)) + 1))/2 * b1*b2)
 
-total = 0
-for i in range(int(n)) :
-	if (0 == i % b1) :
-		total += i
-print "test:  {total}".format(total=total)
 total = 0
 for i in range(int(n)) :
 	if (0 == i % 5 or 0 == i % 3) :
