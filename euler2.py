ec0286744cc2f16dc543c444653ed1ef2a72c4cd
Thomas Hallock <github@thomashallock.com>
2013-05-21 01:10:48 -0400

    changed spacing around. Not using functions where we don't need to.

--- euler2.py
+++ euler2.py
@@ -1,12 +1,11 @@
-def euler2(fib_limit) :
-    fnn = 1
-    fnnn = 1
-    tot = 0
-    while fnnn < fib_limit :
-        fn = fnnn
-        fnnn = fnnn + fnn
-        fnn = fn
-        if (0 == fnnn % 2) :
-            tot += fnnn
-    return tot
-print euler2(4000000)
+fib_limit = 4000000
+fnn = 1
+fnnn = 1
+tot = 0
+while fnnn < fib_limit :
+	fn = fnnn
+	fnnn = fnnn + fnn
+	fnn = fn
+	if (0 == fnnn % 2) :
+		tot += fnnn
+print tot
--- euler3.py
+++ euler3.py
@@ -1,25 +1,24 @@
 import math
-def lf(n, s = None, c = 2) :
-    if None == s :
-        s = [1] * int(math.sqrt(n)) # our sieve
-    c = 2
-    while c < len(s) : # populate the sieve
-        si = c
-        while si < len(s) and 0 == s[si] : # find the next possible cantidate in the sieve
-            # print "we don't need to check {}".format(si)
-            si = si + 1
-        possible_factor = n / si
-        if (n == si) :
-            return n
-        elif 0 == n % si :
-            possible_factor = n / si
-            print "{} is a prime factor of {}, now we need to check if the recip, {} is prime as well".format(si, n, possible_factor)
-            n = possible_factor
-        c = si
-        # print "sieving {}".format(c)
-        while si < len(s) - c :
-            si += c
-            s[si] = 0
-        lpfc = n / c
-        c += 1
-print lf(600851475143)
+n = 600851475143
+c = 2
+s = [1] * int(math.sqrt(n)) # our sieve
+while c < len(s) : # populate the sieve
+	si = c
+	while si < len(s) and 0 == s[si] : # find the next possible cantidate in the sieve
+		# print "we don't need to check {}".format(si)
+		si = si + 1
+	possible_factor = n / si
+	if (n == si) :
+		print n
+		break;
+	elif 0 == n % si :
+		possible_factor = n / si
+		print "{} is a prime factor of {}, now we need to check if the recip, {} is prime as well".format(si, n, possible_factor)
+		n = possible_factor
+	c = si
+	# print "sieving {}".format(c)
+	while si < len(s) - c :
+		si += c
+		s[si] = 0
+	lpfc = n / c
+	c += 1
