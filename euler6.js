3b7c52a79524ab8daf58a0e6daf1f98102434850
Thomas Hallock <github@thomashallock.com>
2014-01-09 21:59:39 -0500

    fixed this forumula

--- euler6.js
+++ euler6.js
@@ -6,4 +6,4 @@ for(i=1; i <= 100; ++i) {
 sqs = Math.pow(sqs,2);
 console.log("" + sqs + " - " + ssq + " = " + (sqs - ssq));
 // 
-// wolfram alpha gives: sum(x^2) + sum(x) ^ 2 from x=1 .. n = 1/12 n (n+1) (3 n+2) (n-1)
+// wolfram alpha gives: sum(x^2) - sum(x) ^ 2 from x=1 .. n = 1/12 n (n+1) (3 n+2) (n-1)
